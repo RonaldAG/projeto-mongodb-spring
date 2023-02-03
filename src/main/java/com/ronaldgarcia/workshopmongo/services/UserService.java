e3888f37f7b13bb932c9c9eedf8540dc48c916da
Ronald Garcia <ronaldag58@gmail.com>
2023-02-03 10:52:51 -0300

    feat: PUT method in user resource

--- src/main/java/com/ronaldgarcia/workshopmongo/services/UserService.java
+++ src/main/java/com/ronaldgarcia/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.ronaldgarcia.workshopmongo.domain.User;
+import com.ronaldgarcia.workshopmongo.dto.UserDTO;
 import com.ronaldgarcia.workshopmongo.repositories.UserRepository;
 import com.ronaldgarcia.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -23,5 +24,13 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> user = repository.findById(id);
 		return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	public User insert(User obj) {
+		return repository.save(obj);
+	}
+	
+	public User fromDTO(UserDTO obj) {
+		return new User(obj.getId(), obj.getName(), obj.getEmail());
 	}
 }
