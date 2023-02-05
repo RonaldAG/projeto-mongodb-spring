a5e933d7e21fa8007190f76c886135346e2a2284
Ronald Garcia <ronaldag58@gmail.com>
2023-02-05 12:31:58 -0300

    feat: endpoint to get a post by id

---
+++ src/main/java/com/ronaldgarcia/workshopmongo/services/PostService.java
@@ -0,0 +1,22 @@
+package com.ronaldgarcia.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ronaldgarcia.workshopmongo.domain.Post;
+import com.ronaldgarcia.workshopmongo.repositories.PostRepository;
+import com.ronaldgarcia.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository repository;
+	
+	public Post findById(String id){
+		Optional<Post> post = repository.findById(id);
+		return post.orElseThrow(() -> new ObjectNotFoundException("Post not found"));
+	}
+}
