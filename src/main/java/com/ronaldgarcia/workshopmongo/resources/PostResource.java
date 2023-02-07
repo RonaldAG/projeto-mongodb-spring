1c5beba42607739567f8ec647bb34a9eeb23882b
Ronald Garcia <ronaldag58@gmail.com>
2023-02-07 20:52:41 -0300

    feat: add a get method to search post by title using @query notation

--- src/main/java/com/ronaldgarcia/workshopmongo/resources/PostResource.java
+++ src/main/java/com/ronaldgarcia/workshopmongo/resources/PostResource.java
@@ -33,4 +33,11 @@ public class PostResource {
 		List<Post> list = service.findByTitle(text);
 		return ResponseEntity.ok(list);
 	}
+	
+	@GetMapping(value = "/searchbytitle")
+	public ResponseEntity<List<Post>> searchTitle(@RequestParam(value="text", defaultValue = "") String text){
+		text = URL.decodeParam(text);
+		List<Post> list = service.searchTitle(text);
+		return ResponseEntity.ok(list);
+	}
 }
